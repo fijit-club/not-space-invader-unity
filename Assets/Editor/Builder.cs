9e6b78cf4b63efe8c3ccba818dc54d7f3a8e1134
KobaltKoding <66021061+KobaltKoding@users.noreply.github.com>
2023-12-21 12:44:51 +0530

    build path added

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -6,7 +6,7 @@ internal class Builder{
     {
 
         // Place all your scenes here
-        string[] scenes = {"path_to_the_scene"};
+        string[] scenes = {"Assets/Editor/MainGame.unity"};
 
         string[] args = Environment.GetCommandLineArgs();
         string outputPath = "builds/raw/web"; // Set a default path if the argument is not provided
