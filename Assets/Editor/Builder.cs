915bd2284239863b54991b06f8827e91340f8f5f
Karrthik Arya <76391390+Karrthik-Arya@users.noreply.github.com>
2023-12-24 17:20:51 +0530

    Update Builder.cs

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -6,7 +6,7 @@ internal class Builder{
     {
 
         // Place all your scenes here
-        string[] scenes = {"Assets/Editor/MainGame.unity"};
+        string[] scenes = {"Assets/Scene/MainGame.unity"};
 
         string[] args = Environment.GetCommandLineArgs();
         string outputPath = "builds/raw/web"; // Set a default path if the argument is not provided
